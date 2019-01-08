6f909eb6a3563508446c35003e26d4b7277fcddc
Diego Moreno <iam@diegomoreno.co>
2019-01-08 18:25:39 -0500

    Build

---
+++ build/precache-manifest.84a846c01f85365176263bad243720b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "427ef9ff2656f0e1c5db",
+    "url": "/static/js/main.427ef9ff.chunk.js"
+  },
+  {
+    "revision": "316de9308ca9077524dd",
+    "url": "/static/js/1.316de930.chunk.js"
+  },
+  {
+    "revision": "427ef9ff2656f0e1c5db",
+    "url": "/static/css/main.5d123ad6.chunk.css"
+  },
+  {
+    "revision": "cddf2e5cf72479a646ccfb2f84024638",
+    "url": "/index.html"
+  }
+];
