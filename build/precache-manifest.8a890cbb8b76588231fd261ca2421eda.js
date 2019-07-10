b10c2748e39ba5f1104cac5f6e1d961448b8ce7c
Diego Hernán Moreno Amaya <diegomorenoa@unisangil.edu.co>
2019-07-09 23:34:14 -0500

    Firebase deploy

---
+++ build/precache-manifest.8a890cbb8b76588231fd261ca2421eda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcb6b784337ce0601aaa",
+    "url": "/static/js/main.bcb6b784.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bcb6b784337ce0601aaa",
+    "url": "/static/css/main.ec4b04ed.chunk.css"
+  },
+  {
+    "revision": "4fe60c6c27252dabb5e6c8feb84960a9",
+    "url": "/index.html"
+  }
+];
