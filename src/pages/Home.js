e5c64c6c3c66569154d4bd527f9f60cee0a397f0
Diego Moreno <iam@diegomoreno.co>
2020-04-08 12:18:29 -0500

    First idea

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,22 +9,33 @@ class HomePage extends Component {
   constructor(state) {
     super(state);
     this.state = {
-
+      typed: true
     }
   }
 
   render() {
+    const { typed } = this.state;
     return (
       <main>
-        <Typed
-          strings={[
-            `Hola a todos,
-            </br>Soy Diego Moreno
-            </br>Desarrollador Full-Stack en Node.js
-            `]}
-          typeSpeed={40}
-          backSpeed={50}
-          className="typed" />
+        {
+          typed
+            ?
+            <Typed
+              strings={[
+                `Hola a todos,
+                </br>Soy Diego Moreno
+                </br>Desarrollador Full-Stack en Node.js
+                `,
+                'Les presento mi portafolio 😁', '']}
+              typeSpeed={40}
+              backSpeed={20}
+              onComplete={() => this.setState({ typed: false })}
+              className="typed" />
+            :
+            <div>
+              <h1 className="animated fadeInUp">Hola</h1>
+            </div>
+        }
       </main>
     )
   }
